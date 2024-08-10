2d21d474b70671ae2de809ecdc3c00e70466189a
IsabellaInglis <156921267+IsabellaInglis@users.noreply.github.com>
2024-08-10 15:40:14 +0100

    imported MUI and changed styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Search from "./Search";
 function App() {
   return (
     <div className="App">
-      <Search />
+      <div className="mainContainer">
+        <h2>What word do you want to look up?</h2>
+        <Search />
+      </div>
     </div>
   );
 }
--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,26 @@
+import { SearchRounded } from "@mui/icons-material";
+import { IconButton, Input } from "@mui/material";
 import React from "react";
+import "./Search.css";
 
-export default function () {
+export default function Search() {
   return (
-    <div>
-      <input type="search" />
-      <input type="submit" />
+    <div className="search-container">
+      <div style={{ height: "80%", width: "90%" }}>
+        <Input
+          className="input"
+          disableUnderline
+          value={"Search for any word..."}
+          endAdornment={
+            <IconButton
+              label="Outlined"
+              style={{ color: "pink", height: "30px", width: "30px" }}
+            >
+              <SearchRounded />
+            </IconButton>
+          }
+        />
+      </div>
     </div>
   );
 }
