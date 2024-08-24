8f341adb450427aa87e76200166a04c6f79574b6
IsabellaInglis <156921267+IsabellaInglis@users.noreply.github.com>
2024-08-24 13:28:12 +0100

    Added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
     <div className="meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       <p>{props.meaning.definition}</p>
+      <p>
+        <Synonyms synonyms={props.meaning.synonyms} />
+      </p>
     </div>
   );
 }
